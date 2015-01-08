918c69a3bf1ebd06a2f724ecfca95ea9f07999ff
Simon Wallmann <s.wallmann@kompsoft.de>
2015-01-08 19:52:30 +0100

    Bump version to 1.0.0

--- eveonline.class.php
+++ eveonline.class.php
@@ -26,7 +26,7 @@ if ( !defined('EQDKP_INC') ){
 if(!class_exists('eveonline')) {
 	class eveonline extends game_generic {
 		protected static $apiLevel	= 20;
-		public $version				= '0.1.1';
+		public $version				= '1.0.0';
 		protected $this_game		= 'eveonline';
 		protected $types			= array('classes', 'factions', 'filters');
 		protected $classes			= array();
